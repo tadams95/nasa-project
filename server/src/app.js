cbd061e93f1737943ae38750e796898c332aabe3
tadams95 <tyrelleadams28@gmail.com>
2022-12-05 17:22:42 -0800

    serve react.js frontend in prod

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -5,10 +6,19 @@ const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendfile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
