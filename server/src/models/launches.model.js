dffbfce3ca77c24bfc90f46020f983241a75d06f
tadams95 <tyrelleadams28@gmail.com>
2023-01-02 14:49:22 -0800

    Create Mongoose schemas for launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -8,7 +8,7 @@ const launch = {
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
   target: "Kepler-442 b",
-  customer: ["RAGESTATE", "NASA"],
+  customers: ["RAGESTATE", "NASA"],
   upcoming: true,
   success: true,
 };
---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,37 @@
+//the code in this file is specific to how this app talks to the Mongo database
+
+const mongoose = require("mongoose");
+
+//schema
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: [String],
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
