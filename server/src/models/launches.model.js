d513fbdd389d5ead2ad0a62c67e0977477a08632
tadams95 <tyrelleadams28@gmail.com>
2023-01-04 14:55:44 -0800

    Schedule new launches, save to MongoDB

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,9 +1,7 @@
 const launchesDatabase = require("./launches.mongo");
 const planets = require("./planets.mongo");
-
+const DEFAULT_FLIGHT_NUMBER = 100;
 const launches = new Map();
-
-let latestFlightNumber = 100;
 
 const launch = {
   flightNumber: 100,
@@ -22,6 +20,16 @@ function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
 
+async function getLatestFlightNumber() {
+  const latestLaunch = await launchesDatabase.findOne().sort("-flightNumber");
+
+  if (!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+
+  return latestLaunch.flightNumber;
+}
+
 async function getAllLaunches() {
   return await launchesDatabase.find({}, { _id: 0, __v: 0 });
 }
@@ -31,8 +39,8 @@ async function saveLaunch(launch) {
     keplerName: launch.target,
   });
 
-  if (!planet){
-    throw new Error("No matching planet found")
+  if (!planet) {
+    throw new Error("No matching planet found");
   }
 
   await launchesDatabase.updateOne(
@@ -46,17 +54,17 @@ async function saveLaunch(launch) {
   );
 }
 
-function addNewLaunch(launch) {
-  latestFlightNumber++;
-  launches.set(
-    latestFlightNumber,
-    Object.assign(launch, {
-      success: true,
-      upcoming: true,
-      customers: ["RAGESTATE", "NASA"],
-      flightNumber: latestFlightNumber,
-    })
-  );
+async function scheduleNewLaunch(launch) {
+  const newFlightNumber = (await getLatestFlightNumber()) + 1;
+
+  const newLaunch = Object.assign(launch, {
+    success: true,
+    upcoming: true,
+    customers: ["RAGESTATE", "NASA"],
+    flightNumber: newFlightNumber,
+  });
+
+  await saveLaunch(newLaunch);
 }
 
 function abortLaunchById(launchId) {
@@ -69,6 +77,6 @@ function abortLaunchById(launchId) {
 module.exports = {
   existsLaunchWithId,
   getAllLaunches,
-  addNewLaunch,
+  scheduleNewLaunch,
   abortLaunchById,
 };
