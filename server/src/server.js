8d4181d231e079f0c25278d9a3967fa2d342e49c
tadams95 <tyrelleadams28@gmail.com>
2023-01-04 16:46:41 -0800

    Update tests to work with MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,28 +1,16 @@
 const http = require("http");
 
-const mongoose = require("mongoose");
-
 const app = require("./app");
 
+const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL =
-  "mongodb+srv://tyrelle:XZ36vDTNTjbCOGqg@cluster0.csas9qx.mongodb.net/nasa";
-
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
