8da918acb52d55921384384f7864b82882ec125f
tadams95 <tyrelleadams28@gmail.com>
2023-01-06 15:04:17 -0800

    Paginated APIs and Minimize load

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -17,8 +17,12 @@ const launch = {
 
 saveLaunch(launch);
 
+async function findLaunch(filter) {
+  return await launchesDatabase.findOne(filter);
+}
+
 async function existsLaunchWithId(launchId) {
-  return await launchesDatabase.findOne({
+  return await findLaunch({
     flightNumber: launchId,
   });
 }
@@ -59,11 +63,12 @@ async function saveLaunch(launch) {
 
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
-async function loadLaunchData() {
+async function populateLaunches() {
   console.log("Downloading launch data...");
   const response = await axios.post(SPACEX_API_URL, {
     query: {},
     options: {
+      pagination: false,
       populate: [
         {
           path: "rocket",
@@ -99,7 +104,25 @@ async function loadLaunchData() {
       customers: customers,
     };
     console.log(`${launch.flightNumber} ${launch.mission}`);
+
+    //TODO: populate launches collection
   }
+}
+
+async function loadLaunchData() {
+  const firstLaunch = await findLaunch({
+    flightNumber: 1,
+    rocket: "Falcon 1",
+    mission: "FalconSat",
+  });
+
+  if (firstLaunch) {
+    console.log("Launch data already loaded");
+  }else {
+    await populateLaunches();
+  }
+
+ 
 }
 
 async function scheduleNewLaunch(launch) {
